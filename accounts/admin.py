3276f20da01d0a9ac9dcb2834596403546044a12
sone <sthaya@gmail.com>
2021-12-15 15:06:08 -0800

    setup crud operations and filtering

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
-
+from .models import Customer, Product, Order, Tag
 # Register your models here.
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone', 'email', 'date_created')
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'category', 'date_created')
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('status', 'date_created')
+
+
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Order, OrderAdmin)
